08fccc20560dd6d439bd8fb9ee72ad4f69db13db
sfazli96 <sfazli96@gmail.com>
2020-10-29 23:40:51 -0700

    Finished with tutorial

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from blog.models import Post, Comment
 from .forms import CommentForm 
 
 def blog_index(request):
-	posts = Post.objects.all().order_by('-created-on')
+	posts = Post.objects.all().order_by('-created_on')
 	context = {
 		"posts": posts,
 	}
